7104410d14d65db97863ce749c38d4d6a430c824
khakes2303 <60640018+khakes2303@users.noreply.github.com>
2021-02-05 17:00:51 +0900

    Add files via upload

---
+++ location1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class location1 : MonoBehaviour
+{
+    public GameObject text;
+    static int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        score = score + 1;
+        print(score);
+        if(score == 5)
+        {
+            SceneManager.LoadScene("scene3");
+        }
+        Destroy(gameObject);
+        Destroy(text);
+        
+
+    }
+}
